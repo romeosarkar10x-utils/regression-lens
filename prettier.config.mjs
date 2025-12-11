463cb10d6935068c843f18330d801846b40333ea
Romeo Sarkar <romeosarkar10x@gmail.com>
2025-12-11 17:57:06 +0530

    Initalized project

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type { import("prettier").Config }
+ */
+
+const config = {
+    trailingComma: "all",
+    tabWidth: 4,
+    printWidth: 80,
+};
+
+export default config;
